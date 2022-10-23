26a39e001fe7c1b75ce6db8f7b8c8d879f53da36
kadirermantr <kadirermantr@gmail.com>
2022-10-23 22:58:11 +0300

    refactor: edit icon class

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <i class="fas fa-rocket"></i>
         </div>
         <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }}</div>
     </a>
