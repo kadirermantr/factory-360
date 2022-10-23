196b195af73e72c470db2bbcaa1602fecd8f94a2
kadirermantr <kadirermantr@gmail.com>
2022-10-23 21:45:55 +0300

    feat: add theme files and templates

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.header')
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        @include('layouts.sidebar')
+
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <div id="content">
+                @include('layouts.top-bar')
+
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">@yield('title')</h1>
+                    @yield('content')
+                </div>
+            </div>
+
+            @include('layouts.footer')
+        </div>
+    </div>
+
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    @include('layouts.js')
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,55 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">{{ config('app.name', 'Laravel') }}</div>
+    </a>
+
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider">
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('user.index') }}">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Users</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('company.index') }}">
+            <i class="fas fa-fw fa-building"></i>
+            <span>Companies</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('employee.index') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Employees</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('industry.index') }}">
+            <i class="fas fa-fw fa-industry"></i>
+            <span>Industries</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
