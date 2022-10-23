d5825991b8483180e5fc9e79d5cf915f50d37040
kadirermantr <kadirermantr@gmail.com>
2022-10-23 22:55:13 +0300

    feat: add view files

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <!-- Page Heading -->
+    @section('title', 'Users')
+
+    <p class="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus eius enim hic laboriosam libero magnam numquam omnis perspiciatis, quam suscipit.
+    </p>
+@endsection
