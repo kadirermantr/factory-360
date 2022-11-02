f7b4b069fd61163ada29e4e9518da0dcc7e005e1
kadirermantr <kadirermantr@gmail.com>
2022-11-02 07:36:23 +0300

    feat: add turkish language files

---
+++ lang/tr/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Girilmiş olan kullanıcı verileri sistemdekiler ile eşleşmemektedir.',
+    'password' => 'Belirtilen şifre yanlış.',
+    'throttle' => 'Çok fazla oturum açma girişiminde bulundunuz. Lütfen :seconds saniye sonra tekrar deneyiz.',
+    'forgot_password' => 'Parolanı mı unuttun?',
+    'login' => 'Giriş',
+    'forgot_password_description' => 'Size bir şifre sıfırlama e-postası gönderelim.',
+    'password_reset' => 'Parolayı sıfırla',
+];
