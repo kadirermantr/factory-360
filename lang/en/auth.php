e701c486a05c3c9154660061b51e7899a1b481a7
kadirermantr <kadirermantr@gmail.com>
2022-11-02 07:37:00 +0300

    feat: add new keywords

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,8 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'forgot_password' => 'Forgot password?',
+    'login' => 'Login',
+    'forgot_password_description' => 'We will email you a password reset.',
+    'password_reset' => 'Reset password',
 ];
