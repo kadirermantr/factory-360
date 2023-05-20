81fc43f4be4aaba241297cbb226f060e6dc52231
kadirerman <kadirermantr@gmail.com>
2023-05-21 00:41:30 +0300

    refactor: add user factory

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,40 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Hash;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $password = Hash::make('p@ssw0rd');
-        $date = now();
-
-        User::insert([
-            [
-                'name' => 'Girift Tech',
-                'email' => 'hr@girift.tech',
-                'password' => $password,
-                'created_at' => $date,
-            ],
-            [
-                'name' => 'Yasin Baran',
-                'email' => 'yasinbaran@girift.tech',
-                'password' => $password,
-                'created_at' => $date,
-            ],
-            [
-                'name' => 'Kadir Erman',
-                'email' => 'hello@kadirerman.com',
-                'password' => $password,
-                'created_at' => $date,
-            ],
-        ]);
+        User::factory()->count(10)->create();
     }
 }
